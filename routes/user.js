884a10cace786c4a4b953d3b240e256f70dbd5e2
Napat Srithavalya <ball04407@hotmail.com>
2023-04-29 21:17:33 +0700

    Add: middleware to  getReservationFromUserId

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,10 @@ const mockupfunc = require('../util/mockup-response.js').mockupfunc;
 const { getReservations } = require('../controllers/user/user.js');
 const router = express.Router();
 
-router.route("/:u_id/reservation").get(getReservations);
+const { protect, authorize } = require('../middleware/auth.js');
+
+
+router.route("/:u_id/reservation").get(protect, authorize('user'), getReservations);
 router.route('/:u_id/reservation/:r_id')
     .put(mockupfunc('update a reservation of a user'))
     .delete(mockupfunc('delete a reservation of a user'))
