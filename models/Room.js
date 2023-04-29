f6ddc49ecc693d5e8d032f4751303862c2a277d9
Napat Srithavalya <ball04407@hotmail.com>
2023-04-29 22:06:19 +0700

    Add: Update reservation api and add mockup json

--- models/Room.js
+++ models/Room.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const RoomSchema = new mongoose.Schema({
-    coworking_id : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'CoWorking',
-        required : [true, "Please add a coworking id"],
-    },
-    name : {
-        type : String,
-        required : [true, "Please add name"],
-    },
-    size : {
-        type : Number,
-        required : [true, "Please add a size"],
-    },
-    price : {
-        type : Number,
-        required : [true, "Please add a price"],
-    },
+  coworking_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CoWorking",
+    required: [true, "Please add a coworking id"],
+  },
+  name: {
+    type: String,
+    required: [true, "Please add name"],
+  },
+  size: {
+    type: String,
+    enum: ["S", "M", "L"],
+    required: [true, "Please add a size"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please add a price"],
+  },
 });
 
-module.exports = mongoose.model('Room', RoomSchema);
+module.exports = mongoose.model("Room", RoomSchema);
