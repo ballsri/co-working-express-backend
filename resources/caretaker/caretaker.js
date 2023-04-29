74f752909daca150917d29fbccb8bb7f32231365
Napat Srithavalya <ball04407@hotmail.com>
2023-04-30 04:43:23 +0700

    Add: simple order and caretaker

---
+++ resources/caretaker/caretaker.js
@@ -0,0 +1,19 @@
+export const caretaker = [
+  { id: 1, name: "น้องแจง", c_id: "644d2ab1d4fc252c58ab1d06", price: 3000 },
+  { id: 2, name: "น้องเตย", c_id: "644d2ab1d4fc252c58ab1d06", price: 2500 },
+  { id: 3, name: "น้องกาย", c_id: "644d2ab1d4fc252c58ab1d06", price: 2300 },
+  { id: 4, name: "น้องจอย", c_id: "644d2ab1d4fc252c58ab1d06", price: 4000 },
+  { id: 5, name: "น้องเบล", c_id: "644d2ab1d4fc252c58ab1d06", price: 3500 },
+  {
+    id: 6,
+    name: "น้องอุ๋งอิ๋ง",
+    c_id: "644d2ab1d4fc252c58ab1d06",
+    price: 3000,
+  },
+  { id: 7, name: "น้องตูมตาม", c_id: "644d2abffc157b36a3417abe", price: 1500 },
+  { id: 8, name: "น้องจุ๊บแจง", c_id: "644d2abffc157b36a3417abe", price: 3500 },
+  { id: 9, name: "น้องกิ๊บ", c_id: "644d2abffc157b36a3417abe", price: 2500 },
+  { id: 10, name: "น้องวาดฝัน", c_id: "644d2abffc157b36a3417abe", price: 2000 },
+  { id: 11, name: "น้องจุ๋ม", c_id: "644d2abffc157b36a3417abe", price: 1700 },
+  { id: 12, name: "น้องแอลลี่", c_id: "644d2abffc157b36a3417abe", price: 4000 },
+];
